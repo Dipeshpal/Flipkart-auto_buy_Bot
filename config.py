dd0700be9464261d01f2845fdea1b930a85617b5
Dipesh Paul <dipeshpal17@gmail.com>
2018-10-27 11:29:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 keys = {
         "product_url": "https://www.flipkart.com/samsung-galaxy-on6-black-64-gb/p/itmf6m35fhyqhtzv?pid=MOBF6FZ4QMNCA48H&lid=LSTMOBF6FZ4QMNCA48H4UVXKS&fm=neo%2Fmerchandising&iid=M_e3b899ca-65ef-49d8-941e-f7a0d7b7a696_2.NRHWMUC0W072&otracker=hp_omu_Top%2BOffers_1_Flat%2B%25E2%2582%25B93%252C500%2BOff_NRHWMUC0W072_0&cid=NRHWMUC0W072",
-        "email": "dipeshpal17@gmail.com",
+        "email": "youremail@domain",
         "password": "Enter Password"
 }
